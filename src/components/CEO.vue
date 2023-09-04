498216fc6dc85dbcee2d3b64da99fea49a64aa05
Javedkhanz <javed49818@gmail.com>
2023-09-04 05:25:07 -0700

    complete

--- src/components/CEO.vue
+++ src/components/CEO.vue
@@ -1,15 +1,15 @@
 <!-- src/components/CEOProfile.vue -->
 
 <template>
-    <div class="container  max-w-6xl p-4 md:p-8 my-3 mx-2 bg-white shadow-lg rounded-lg">
-        <div class="flex items-center">
-            <img src="/hajji_barat.jpg" alt="CEO Image" class="w-16 h-16 rounded-full mr-4" />
+    <div class="container mx-auto  px-4 py-8 bg-white shadow-lg rounded-lg">
+        <div class="flex items-center space-x-4">
+            <img src="/hajji_barat.jpg" alt="CEO Image" class="w-20 h-20 rounded-full" />
             <div>
-                <h1 class="text-3xl font-semibold text-gray-800">Hajji Barat Khan</h1>
-                <p class="text-gray-600">Chief Executive of Hajj Services Company Pvt Ltd.</p>
+                <h1 class="text-4xl font-bold text-gray-900">Hajji Barat Khan</h1>
+                <p class="text-gray-700">Chief Executive of Hajj Services Company Pvt Ltd.</p>
             </div>
         </div>
-        <p class="mt-6 text-gray-700 text-lg leading-relaxed">
+        <p class="mt-8 text-gray-800 text-xl leading-relaxed">
             Welcome to Hajj Services Company Pvt Ltd,
 
             I am honored to extend my warmest greetings to all our valued clients and partners. As the Chief Executive, it
@@ -20,6 +20,7 @@
             the significance of this spiritual journey and are here to provide you with the highest level of service and
             support.
 
+        <div class="hidden md:block">
             As we embark on this sacred pilgrimage together, I encourage you to embrace the spiritual essence of Hajj,
             reflect on its profound significance, and connect with your fellow pilgrims. May your journey be filled with
             blessings, forgiveness, and moments of deep spirituality.
@@ -28,12 +29,14 @@
             are here to serve you with dedication and compassion.
 
             Wishing you a blessed and spiritually enriching Hajj experience.
-
-            <span class="block mt-4 text-gray-800 font-semibold">Sincerely,</span>
-            <span class="block">Haji Barat</span>
-            <span class="block">Chief Executive Officer</span>
-            <span class="block">Hajj Services Company Pvt Ltd</span>
+        </div>
         </p>
+        <div class="mt-8 flex justify-end items-center space-x-2">
+            <span class="text-gray-900 font-bold">Sincerely,</span>
+            <span class="text-gray-900">Haji Barat</span>
+            <span class="text-gray-900">Chief Executive Officer</span>
+            <span class="text-gray-900">Hajj Services Company Pvt Ltd</span>
+        </div>
     </div>
 </template>
   
