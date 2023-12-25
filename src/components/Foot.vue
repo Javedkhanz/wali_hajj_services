74be7563544095653eccc8a9beebb8cc1b039bfc
Javedkhanz <javed49818@gmail.com>
2023-12-25 19:07:58 +0500

    added footer

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -8,19 +8,24 @@ defineProps({
 </script>
 
 <template>
-   <footer class="bg-green-600 py-8 px-8 text-white">
+  <footer class="bg-green-600 py-8 px-8 text-white">
     <div class="container mx-auto flex flex-wrap justify-between items-center">
 
       <!-- A column with the company address and phone number -->
       <div class="w-full md:w-auto mb-4 md:mb-0">
         <p class="mb-2"><strong>Address:</strong></p>
-        <p>Sardar Wali Plaza New Truck Adda ,Near Satellite Pearl Institut Town, Quetta, Balochistan</p>
+        <p><a
+            href="https://maps.google.com?q=Sardar+Wali+Plaza+New+Truck+Adda,+Near+Satellite+Pearl+Institut+Town,+Quetta,+Balochistan"
+            target="_blank">Sardar Wali Plaza New Truck Adda, Near Satellite Pearl Institut Town, Quetta, Balochistan</a>
+        </p>
         <p class="mt-4"><strong>Phone:</strong></p>
-        <p>03003823655</p> 
-        <p>03003829155</p>
+        <p><a href="tel:+923003823655">0300 3823655</a></p>
+        <p><a href="tel:+923003829155">0300 3829155</a></p>
+
         <p class="mt-4"><strong>Email:</strong></p>
-        <p>walihajj5115@gamil.com</p> 
+        <p><a href="mailto:walihajj5115@gmail.com">walihajj5115@gmail.com</a></p>
       </div>
+
       <!-- A column with the company logo and name -->
       <div class="w-full md:w-auto">
         <img src="/logo.jpeg" alt="Wali Hajj Services Company Pvt Ltd" class="h-16 rounded-2xl w-auto mb-4">
@@ -28,7 +33,6 @@ defineProps({
       </div>
     </div>
   </footer>
-
 </template>
 
 <style scoped></style>
